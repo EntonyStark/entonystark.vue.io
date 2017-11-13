74ec8e6d531ec4e2d652125c1c3c4962d11a6a08
Entony <leonskottkenedi@gmail.com>
2017-11-13 17:50:37 +0200

    add some info

--- src/components/section.vue
+++ src/components/section.vue
@@ -2,17 +2,17 @@
   <div class="row">
     <div class="col-3">
       <div class="card ">
-        <img class="card-img-top rounded float-left" :src="nav.navigation[index].Src" alt="Card image cap">
+        <img class="card-img-top rounded float-left" :src="nav.navigation[index].Src" alt="Card image cap"></img>
        <div class="card-body">
         <h4 class="card-title">{{nav.navigation[index].Title}}</h4>
-        <p class="card-text">Not intresting text</p>
-        <a href="#" class="btn btn-primary">Go out</a>
+        <p class="card-text">Click to get information</p>
+        <a href="#" class="btn btn-primary" @click.prevent="newPage(nav, index)">More</a>
       </div>
 </div>
     </div>
-    <div>
-      <h1>{{nav.navigation[index].Title}}</h1>
-      <p>{{nav.navigation[index].Description}}</p>
+    <div class="col-7">
+      <h1>Some info: </h1>
+      <p>{{nav.navigation[index].Description[counter].someText}}</p>
     </div>
   </div>
 </template>
@@ -23,12 +23,21 @@ export default {
   name: 'thearticle',
   data () {
     return {
+      counter: 0
     }
   },
   computed: {
     ...mapGetters({
       nav: 'getNav'
     })
+  },
+  methods: {
+    newPage (nav, index) {
+      this.counter++
+      if (this.counter === nav.navigation[index].Description.length) {
+        this.counter = 0
+      }
+    }
   },
   props: {
     index: { type: Number, require: true }
